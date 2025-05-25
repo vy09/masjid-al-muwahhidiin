f71ecdf77c235f15e97173a5ea40c50bbc2e3f43
Luvy Muhammad <luvymuhammad09@gmail.com>
2025-05-25 13:02:30 +0700

    Increase maximum length for 'deskripsi' field validation in store and update methods

--- app/Http/Controllers/KegiatanMasjidController.php
+++ app/Http/Controllers/KegiatanMasjidController.php
@@ -33,7 +33,7 @@ class KegiatanMasjidController extends Controller
             'waktu_mulai' => 'required|date_format:H:i',
             'waktu_selesai' => 'required|date_format:H:i|after:waktu_mulai',
             'tempat_kegiatan' => 'required|string|max:255',
-            'deskripsi' => 'required|string|max:255',
+            'deskripsi' => 'required|string|max:99999',
             'gambar' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
@@ -71,7 +71,7 @@ class KegiatanMasjidController extends Controller
             'tanggal_kegiatan' => 'required|date',
             'waktu_mulai' => 'required|date_format:H:i',
             'waktu_selesai' => 'required|date_format:H:i|after:waktu_mulai',
-            'tempat_kegiatan' => 'required|string|max:255',
+            'tempat_kegiatan' => 'required|string|max:99999',
             'gambar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
