49244f3242acf0fb3697d5526312382e7c597e8b
Luvy Muhammad <luvymuhammad09@gmail.com>
2025-12-02 19:24:00 +0700

    Refactor Vercel PHP runtime configuration and update entry point

--- api/index.php
+++ api/index.php
@@ -1,19 +1,3 @@
 <?php
-// Vercel PHP runtime entry point for Laravel
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require __DIR__ . '/../vendor/autoload.php';
-} else {
-    require __DIR__ . '/vendor/autoload.php';
-}
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
